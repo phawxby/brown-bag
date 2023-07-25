4e78110e166affdad73f50ba3da63318fb29ee80
Paul Hawxby <paul.hawxby@myenergi.com>
2023-07-25 14:56:04 +0100

    feat: flaky test

--- src/bag-01/01-basic-test/index.test.ts
+++ src/bag-01/01-basic-test/index.test.ts
@@ -1,6 +1,10 @@
-import doSomething from ".";
-import { test, expect } from "vitest";
+import doSomething from '.';
+import { test, expect } from 'vitest';
 
-test("simple test", () => {
-  expect(doSomething("foo")).toEqual("---foo---");
+test('simple test', () => {
+  expect(doSomething('foo')).toEqual('---foo---');
+});
+
+test('flakey test', () => {
+  expect(Math.random()).toBeLessThan(0.75);
 });
